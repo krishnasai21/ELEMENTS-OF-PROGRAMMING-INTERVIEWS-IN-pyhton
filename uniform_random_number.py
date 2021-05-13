c30176cdba882db4141d3e1a26cde50edac84f2d
Krishna Pala <palakrishnasai@gmail.com>
2021-05-13 20:07:14 +0530

    Generate Unifrom Random Numbers
    As this problem is motivated by the following, Six friends have to select a designated driver using a single unbaised coin. The Process should be fair to everyone

---
+++ uniform_random_number.py
@@ -0,0 +1,11 @@
+def uniform_random(lower_bound: int, upper_bound: int) -> int:
+number_of_outcomes = upper_bound - lower_bound + 1
+while True:
+  result, i = 0, 0
+  while (1 << i) < number_of_outcomes:
+    # zero_one_random() is the provided random number generator
+    result = (result << 1) | zero_one_random()
+    i +=1
+    if result < number_of_outcomes:
+      break
+      return result + lower_bound
